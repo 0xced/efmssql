c6ba1bd6d84559ba7fb882e43fa7c61f21732dea
Cédric Luthi <cedric.luthi@gmail.com>
2026-08-25 16:24:31 +0200

    Make tests less flaky
    Asserting on the exact SqlClient exceptions is not reliable enough, even with precise timing based on SqlClient internal logs.

--- tests/RecordRetryingStrategy.cs
+++ tests/RecordRetryingStrategy.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 
@@ -7,12 +9,19 @@ namespace WorkaroundSqlClientIssue26.Tests;
 
 public sealed class RecordRetryingStrategy(ExecutionStrategyDependencies dependencies) : SqlServerRetryingExecutionStrategy(dependencies)
 {
-    public readonly List<bool> ShouldRetry = [];
+    public readonly List<(Exception Exception, bool ShouldRetry)> Retries = [];
+    public bool Executed { get; private set; }
+
+    public override async Task<TResult> ExecuteAsync<TState, TResult>(TState state, Func<DbContext, TState, CancellationToken, Task<TResult>> operation, Func<DbContext, TState, CancellationToken, Task<ExecutionResult<TResult>>>? verifySucceeded, CancellationToken cancellationToken = new CancellationToken())
+    {
+        Executed = true;
+        return await base.ExecuteAsync(state, operation, verifySucceeded, cancellationToken);
+    }
 
     protected override bool ShouldRetryOn(Exception exception)
     {
-        var result = base.ShouldRetryOn(exception);
-        ShouldRetry.Add(result);
-        return result;
+        var shouldRetry = base.ShouldRetryOn(exception);
+        Retries.Add((exception, shouldRetry));
+        return shouldRetry;
     }
 }
--- tests/WorkaroundSqlClientIssue26Test.cs
+++ tests/WorkaroundSqlClientIssue26Test.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
@@ -10,35 +11,40 @@ namespace WorkaroundSqlClientIssue26.Tests;
 
 public class WorkaroundSqlClientIssue26Test(ITestOutputHelper output, MsSqlFixture fixture) : IClassFixture<MsSqlFixture>
 {
+    private static readonly Type[] ExpectedSqlClientErroneousCancellationExceptionTypes = [typeof(TaskCanceledException), typeof(SqlException), typeof(InvalidOperationException)];
+
     [SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "Required for xUnit")]
-    public static TheoryData<string, Type> TestData = new()
-    {
-        { "SqlConnection.InternalOpenAsync", typeof(TaskCanceledException) },
-        { "SqlCommand.BeginExecuteReaderInternalReadStage", typeof(SqlException) },
-        { "SqlCommand.WriteBeginExecuteEvent", typeof(InvalidOperationException) },
-    };
+    public static TheoryData<string> TestData =
+    [
+        "SqlConnection.InternalOpenAsync",
+        "SqlCommand.BeginExecuteReaderInternalReadStage",
+        "SqlCommand.WriteBeginExecuteEvent",
+    ];
 
     [Theory, MemberData(nameof(TestData))]
-    public async Task TestWorkaround(string cancellationMessage, Type expectedInnerExceptionType)
-        => await ExecuteTestAsync(cancellationMessage, expectedInnerExceptionType, sql => sql.WorkAroundSqlClientIssue26());
+    public async Task TestWorkaround(string cancellationMessage)
+        => await ExecuteTestAsync(cancellationMessage, sql => sql.WorkAroundSqlClientIssue26());
 
     [Theory, MemberData(nameof(TestData))]
-    public async Task TestRetryOnFailureAndWorkaround(string cancellationMessage, Type expectedInnerExceptionType)
-        => await ExecuteTestAsync(cancellationMessage, expectedInnerExceptionType, sql => sql.EnableRetryOnFailure().WorkAroundSqlClientIssue26());
+    public async Task TestRetryOnFailureAndWorkaround(string cancellationMessage)
+        => await ExecuteTestAsync(cancellationMessage, sql => sql.EnableRetryOnFailure().WorkAroundSqlClientIssue26());
 
     [Theory, MemberData(nameof(TestData))]
-    public async Task TestRecordRetryOnFailureAndWorkaround(string cancellationMessage, Type expectedInnerExceptionType)
+    public async Task TestRecordRetryOnFailureAndWorkaround(string cancellationMessage)
     {
         RecordRetryingStrategy strategy = null!;
-        await ExecuteTestAsync(cancellationMessage, expectedInnerExceptionType, sql => sql.ExecutionStrategy(d => strategy = new RecordRetryingStrategy(d)).WorkAroundSqlClientIssue26());
-        if (strategy.ShouldRetry.Count > 0)
+        await ExecuteTestAsync(cancellationMessage, sql => sql.ExecutionStrategy(d => strategy = new RecordRetryingStrategy(d)).WorkAroundSqlClientIssue26());
+        Assert.True(strategy.Executed);
+
+        // There's unfortunately little to assert on this since the cancellation exceptions are rather random, so they are just logged
+        foreach (var (exception, shouldRetry) in strategy.Retries)
         {
-            // Getting a SqlException is not 100% reliable; sometimes the exception is thrown from EF Core, not SqlClient.
-            Assert.False(Assert.Single(strategy.ShouldRetry));
+            var sqlErrors = (exception as SqlException)?.Errors.Cast<SqlError>().Select(e => e.Number) ?? [];
+            output.WriteLine($"🔁 ShouldRetry: {shouldRetry}, Errors: {string.Join(", ", sqlErrors)} Exception: {exception}");
         }
     }
 
-    private async Task ExecuteTestAsync(string cancellationMessage, Type expectedInnerExceptionType, Action<SqlServerDbContextOptionsBuilder> sqlServerOptionsAction)
+    private async Task ExecuteTestAsync(string cancellationMessage, Action<SqlServerDbContextOptionsBuilder> sqlServerOptionsAction)
     {
         using var cancellationEventListener = new CancellationEventListener(output, cancellationMessage);
         var cancellationToken = cancellationEventListener.CancellationToken;
@@ -47,12 +53,18 @@ public class WorkaroundSqlClientIssue26Test(ITestOutputHelper output, MsSqlFixtu
 
         var exception = await Record.ExceptionAsync(() => context.Tracks.CountAsync(cancellationToken));
 
-        output.WriteLine($"Recorded exception: {exception}");
-        Assert.Equal(cancellationToken, Assert.IsType<OperationCanceledException>(exception, exactMatch: false).CancellationToken);
-        if (expectedInnerExceptionType != typeof(SqlException))
+        output.WriteLine($"💥 Recorded exception from {exception?.Source}: {exception}");
+        var operationCanceledException = Assert.IsType<OperationCanceledException>(exception, exactMatch: false);
+        Assert.Equal(cancellationToken, operationCanceledException.CancellationToken);
+
+        // The exception we get is not 100% reliable; sometimes the exception is thrown from EF Core, sometimes from SqlClient.
+        // When it's coming from EF Core, the FixSqlClientIssue26ExecutionStrategy can't catch it
+        if (operationCanceledException.Source == "WorkaroundSqlClientIssue26")
         {
-            // Getting a SqlException is not 100% reliable, sometimes an InvalidOperationException or a TaskCanceledException might be thrown instead.
-            Assert.IsType(expectedInnerExceptionType, exception.InnerException);
+            // When it's coming from SqlClient, ensure the erroneous exception is saved in the inner exception
+            output.WriteLine($"💥 Inner exception from {exception.InnerException?.Source}: {exception.InnerException}");
+            Assert.NotNull(exception.InnerException);
+            Assert.Contains(exception.InnerException.GetType(), ExpectedSqlClientErroneousCancellationExceptionTypes);
         }
     }
 }
